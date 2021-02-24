df5741460e436327c6303e98f319d5d4e14d5415
Chehui Chou <deadshot465@gmail.com>
2021-02-24 21:52:26 +0900

    Update presences.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,40 @@
 import ackcord._
 import ackcord.commands.PrefixParser
+import ackcord.data.PresenceStatus
+import ackcord.data.raw.RawActivity
+import ackcord.gateway.{GatewayIntents, StatusData, StatusUpdate}
+import akka.stream.scaladsl.Source
+
+import java.time.Instant
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
 
 object Main {
   def main(args: Array[String]): Unit = {
     val token = System.getenv("TOKEN")
-    val clientSettings = ClientSettings(token)
+    val presences = Vector("Demon Hunting", "Cursed Energy", "Divergent Fist", "Black Flash", "Slaughter Demon", "Manga", "Game", "Practising Jujutsu")
+    val initialPresence = presences(Random.between(0, presences.length))
+    val clientSettings = ClientSettings(token, activity = Some(RawActivity(initialPresence, 0, None, Instant.now(), None, None,
+      None, None, None, None, None)), intents = GatewayIntents.All)
+
     import clientSettings.executionContext
 
     clientSettings.createClient()
       .foreach { client =>
+        import client.system
         client.onEventSideEffectsIgnore {
-          case APIMessage.Ready(cache) => printf("%s#%s is now online.\n", cache.current.botUser.username, cache.current.botUser.discriminator)
+          case APIMessage.Ready(cache) =>
+            printf("%s#%s is now online.\n", cache.current.botUser.username, cache.current.botUser.discriminator)
+            client.events.sendGatewayPublish.runWith(Source.tick(1.second, 1.hour, ())
+              .map(_ => StatusUpdate(StatusData(
+                None,
+                Some(
+                  RawActivity(presences(Random.between(0, presences.length)),
+                    0,
+                    None,
+                    Instant.now(),
+                    None, None, None, None, None, None, None)
+                ), PresenceStatus.Online, afk = false))))
         }
 
         // import client.requestsHelper._
